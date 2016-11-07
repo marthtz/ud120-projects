c87aded761b6b3fbc48185bf058282bc3168875f
Unknown <mhintz@posteo.net>
2016-11-07 16:00:13 +1030

    Project 1 test completed

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,6 +27,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+### Project 1
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
+### Project 1 end
 
 #########################################################
 
