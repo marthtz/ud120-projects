3790f62d3574f49ca2165e2bb47717e53561db64
martin <mhintz@posteo.net>
2016-11-15 13:57:26 +1030

    Finished lesson 6 - regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,15 +21,25 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
+
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
+
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
+#print data
+
+
+# Cohda for Python 3.x
+#data2 = featureFormat( dictionary, features_list, remove_any_zeroes=True, sort_keys = '../tools/python2_lesson06_keys.pkl')
+#print data2
+
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,7 +48,16 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+
+print('Slope: {}'.format(reg.coef_))
+print('Intercept: {}'.format(reg.intercept_))
+
+print('Train score: {}'.format(reg.score(feature_train, target_train)))
+print('Test score: {}'.format(reg.score(feature_test, target_test)))
 
 
 
@@ -64,6 +83,12 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+
+print('Slope: {}'.format(reg.coef_))
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
